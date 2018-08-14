f861c71ec861b227f802028140ea0d84038ba2a9
Nicholas  Luong <nckluong@gmail.com>
2018-08-14 00:44:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,9 @@ def line(deli)
   if deli.size == 0 
     puts "The line is currently empty."
   else 
-    puts ""
+    output = "The line is currently:"
+    deli.each do |name|
+      output += " #{deli.index"
   end 
 end 
   
