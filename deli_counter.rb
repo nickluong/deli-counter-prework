6711c351023943f4afde0cfa46a4c82391ad868a
Nicholas  Luong <nckluong@gmail.com>
2018-08-14 00:57:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,16 +2,15 @@
 katz_deli = []
 
 def line(deli)
-      line = []   
-  if deli.size == 0 
-     "The line is currently empty."
-     line
+  if(deli.size == 0)
+    puts "The line is currently empty."
+  end
   else 
-    line << "The line is currently:"
-    deli.each do |name|
-      line << " #{deli.index(name)} #{name}"
+    count = 0 
+    output = "The line is currently:"
+    until count == deli.size
+      count += 1
+      output += "#{count} #{deli[count-1]}"
+    end 
   end 
 end 
-  line 
-end 
-  
